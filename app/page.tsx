2f79ec48fe735dc03fc7b016d4d7e1e4941e7580
Aligorithm-97 <atemurtas97@gmail.com>
2023-10-29 14:41:17 +0300

    aligorithm/scrollSnapAdded

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <div className="container mt-24 mx-auto py-4 px-12">
+      <div className="container mt-24 mx-auto py-4 px-12 h-screen">
         <HeroSection />
         <Projects />
         <Experience />
